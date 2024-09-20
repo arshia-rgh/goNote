119a2ba459ee365e4032821b89e9449f3b671324
arshia <arshiarezagholi1212@gmail.com>
2024-09-20 17:46:18 +0330

    added Note instructer method

--- note/note.go
+++ note/note.go
@@ -1,6 +1,26 @@
 package note
 
+import "errors"
+
 type Note struct {
 	title   string
 	content string
+}
+
+func (*Note) New(title, content string) (*Note, error) {
+	if len(title) < 2 {
+		err := errors.New("note title cant be lower than 2 character")
+		return nil, err
+	}
+	if len(content) < 5 {
+		err := errors.New("note content cant be lower than 5 characters")
+
+		return nil, err
+	}
+
+	return &Note{
+		title:   title,
+		content: content,
+	}, nil
+
 }
