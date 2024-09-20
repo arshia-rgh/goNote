aa71f472fc8f24b09993d184ac650c27cefd1a51
arshia <arshiarezagholi1212@gmail.com>
2024-09-20 18:49:48 +0330

    completed WriteTOFIle func

--- utils/fileOps.go
+++ utils/fileOps.go
@@ -1,7 +1,23 @@
 package utils
 
-func WriteToFile() {
+import (
+	"encoding/json"
+	"goNote/note"
+	"os"
+)
 
+func WriteToFile(fileName string, note note.Note) error {
+	data, err := json.Marshal(note)
+
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(fileName, data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func ReadFromFile() {
