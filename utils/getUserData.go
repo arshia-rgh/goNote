22d2f4b9d1366de8fbc09d1cae65489bd535d78c
arshia <arshiarezagholi1212@gmail.com>
2024-09-20 18:01:20 +0330

    added GetUserData function

--- utils/getUserData.go
+++ utils/getUserData.go
@@ -1,1 +1,16 @@
 package utils
+
+import "fmt"
+
+func GetUserData(prompt string) (string, error) {
+	fmt.Println(prompt)
+	var input string
+	_, err := fmt.Scanln(&input)
+
+	if err != nil {
+		return "", err
+	}
+
+	return input, nil
+
+}
